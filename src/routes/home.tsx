2bc55da90f025bea826da4d568013d00c9f0e34d
Jaewon Shin <west1288@gmail.com>
2024-02-02 18:21:54 -0500

    query data

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import PostTweetForm from "../components/post-tweet-form";
+import Timeline from "../components/timeline";
 const Wrapper = styled.div``;
 
 export default function Home(){
     return (
         <Wrapper>
             <PostTweetForm/>
+            <Timeline/>
         </Wrapper>
     );
 }
