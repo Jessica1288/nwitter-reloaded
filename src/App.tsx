c21f17f5fe66368998417aafa067f0c3dce10b0f
Jaewon Shin <west1288@gmail.com>
2024-02-01 21:20:44 -0500

    Forgot password

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
 import { auth } from "./firebase";
 import ProtectedRoute from "./components/protected-route";
+import ForgtPassword from "./routes/forgot-password";
 
 const router = createBrowserRouter([
   {
@@ -36,6 +37,10 @@ const router = createBrowserRouter([
   {
     path:"/create-account",
     element:<CreateAccount/>
+  },
+  {
+    path:"/forgot-password",
+    element:<ForgtPassword/>
   }
 ]);
 
--- src/routes/forgot-password.tsx
+++ src/routes/forgot-password.tsx
@@ -2,32 +2,27 @@ import React, { useState } from "react";
 import { auth } from "../firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail } from "firebase/auth";
 import { Error, Form, Input, Switcher, Title, Wrapper } from "../components/auth-component";
-import GithubButton from "../components/github-btn";
 
-export default function LogingAccount(){
+export default function ResetAccount(){
     const navigator = useNavigate();
     const[isLoading, setLoading] = useState(false);
     const[email, setEmail] = useState("");
-    const[password, setPassword] = useState("");
     const[error, setError] = useState("");
     const onChange = (e : React.ChangeEvent<HTMLInputElement>) =>{
         const {target : {name, value}} = e;
         if(name === "email"){
             setEmail(value);
         }
-        else if(name === "password"){
-            setPassword(value);
-        }
     };
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setError("");
         try{
-            if(isLoading || email === "" || password === "") return;
+            if(isLoading || email === "") return;
             setLoading(true);
-            await signInWithEmailAndPassword(auth, email, password);
+            await sendPasswordResetEmail(auth, email);
             navigator("/");
         }catch(e){
             if(e instanceof FirebaseError)
@@ -39,19 +34,17 @@ export default function LogingAccount(){
     };
 
     return <Wrapper>
-        <Title>Log into 𝕏</Title>
+        <Title>Reset Password</Title>
         <Form onSubmit={onSubmit}>
             <Input name="email" onChange={onChange} value={email} placeholder="Email" type="eamil" required/>
-            <Input name="password" onChange={onChange} value={password} placeholder="Password" type="password" required/>
-            <Input type="submit" value={isLoading ? "Loading..." : "Log in"}/>
+            <Input type="submit" value={isLoading ? "Loading..." : "Reset"}/>
         </Form>
         {error !== "" ? <Error>{error} </Error> : null }
         <Switcher>
             Don't have an account? <Link to="/create-account">Create one &rarr;</Link>
         </Switcher>
         <Switcher>
-            <Link to="/forgot-password">Forgot your password? </Link>
+            Already have an account? <Link to="/login">Log in &rarr;</Link>
         </Switcher>
-        <GithubButton/>
     </Wrapper>
 }
