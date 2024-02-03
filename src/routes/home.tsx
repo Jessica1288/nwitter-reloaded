954ddfd03e964c8b23f084b45ad2f4298acb1e3c
Jaewon Shin <west1288@gmail.com>
2024-02-03 00:30:21 -0500

    home wrapper style

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 import PostTweetForm from "../components/post-tweet-form";
 import Timeline from "../components/timeline";
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: grid;
+    gap: 50px;
+    grid-template-rows: 1fr 5fr;
+    //overflow-y: scroll;
+`;
 
 export default function Home(){
     return (
