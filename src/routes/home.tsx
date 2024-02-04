ca7326c275dc5c35317781ef9ac93e9a9366d8a1
Jaewon Shin <west1288@gmail.com>
2024-02-04 16:35:37 -0500

    Delete Tweet

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,8 +4,8 @@ import Timeline from "../components/timeline";
 const Wrapper = styled.div`
     display: grid;
     gap: 50px;
-    grid-template-rows: 1fr 5fr;
     //overflow-y: scroll;
+    grid-template-rows: 1fr 5fr;  
 `;
 
 export default function Home(){
